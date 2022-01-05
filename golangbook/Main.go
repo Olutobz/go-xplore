cc3d3fd59a314a5d83ff3c4a478fadb5f1ffb5f8
Onikoyi Damola Olutoba <Damexxey94@gmail.com>
2022-01-05 23:27:30 +0100

    add method to convert ft to metres

--- golangbook/Main.go
+++ golangbook/Main.go
@@ -8,22 +8,50 @@ func main() {
 	fmt.Println(len("Olutoba"))
 	fmt.Println("Hello World"[1])
 
-	hello := "Hello, Olutoba"
-	fmt.Println(hello)
+	name := "Olutoba"
+	age := 24
+	fmt.Println(name, "is", age, "years old.")
+	greetings := "Welcome back, Olutoba"
+	fmt.Println(greetings)
 	golang := "Go is an awesome programming language"
 	fmt.Println(golang)
 
-	laptopName := "I want to own a macbook pro"
-	fmt.Println(laptopName)
+	wishlist := "I want to own a macbook pro and get a software engineering internship"
+	fmt.Println(wishlist)
 
 	const year = 2022
 	fmt.Println(year)
 
 	var (
 		companies = "google, apple, twitter, meta, delivery hero"
-		location = "usa, canada, uk, germany"
+		location  = "usa, canada, uk, germany"
 	)
 	fmt.Println(companies)
 	fmt.Println(location)
 
+	fmt.Print("Enter a number: ")
+	var input float64
+	fmt.Scanf("%f", &input)
+	fmt.Println(input)
+
+	fmt.Print("Enter fahrenheit value:")
+	var fValue float64
+	fmt.Scanf("%f", &fValue)
+	fahrenheitToCelsius(fValue)
+
+	feetToMetres(1)
+
+}
+
+func fahrenheitToCelsius(fahrenheit float64) {
+	// (C = (F − 32) * 5/9).
+	var celsius float64
+	celsius = (fahrenheit - 32) * 5 / 9
+	fmt.Println(fahrenheit, "fahrenheit is", celsius, "celsius")
+}
+
+func feetToMetres(ft float64) {
+	// (1 ft = 0.3048 m)
+	x := ft * 0.3048
+	fmt.Println(ft, "feet is", x, "metres")
 }
